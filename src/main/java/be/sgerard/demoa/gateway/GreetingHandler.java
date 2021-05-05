c2ff0962920c8aa7fbe5f4d438fe2e0f7393e75d
Sébastien Gérard <me@sgerard.be>
2021-05-05 21:20:29 +0200

    database connection

--- src/main/java/be/sgerard/demoa/gateway/GreetingHandler.java
+++ src/main/java/be/sgerard/demoa/gateway/GreetingHandler.java
@@ -1,5 +1,6 @@
 package be.sgerard.demoa.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
@@ -7,11 +8,35 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 @Component
 public class GreetingHandler {
 
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
     public Mono<ServerResponse> hello(ServerRequest request) {
+        final String checkResult = checkConnection();
+
         return ServerResponse.ok().contentType(MediaType.TEXT_PLAIN)
-                .body(BodyInserters.fromValue("Hello, Spring!"));
+                .body(BodyInserters.fromValue("Hello, Spring! Database check result [" + checkResult + "]."));
+    }
+
+    private String checkConnection() {
+        try {
+            DriverManager.getConnection(url, username, password);
+
+            return "success";
+        } catch (SQLException e) {
+            return e.getMessage();
+        }
     }
 }
--- src/test/java/be/sgerard/demoa/gateway/GatewayServiceApplicationTests.java
+++ src/test/java/be/sgerard/demoa/gateway/GatewayServiceApplicationTests.java
@@ -2,12 +2,14 @@ package be.sgerard.demoa.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GatewayServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
